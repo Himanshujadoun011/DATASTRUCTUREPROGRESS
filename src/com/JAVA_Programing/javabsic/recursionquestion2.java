4313ecad66b27f001cb4deba48bd3390543fcf91
himanshu <Himanshu.jadoun2024@glbajajgroup.org>
2026-06-18 11:19:29 +0530

    dsa

--- src/com/JAVA_Programing/javabsic/recursionquestion2.java
+++ src/com/JAVA_Programing/javabsic/recursionquestion2.java
@@ -1,5 +1,7 @@
 package com.JAVA_Programing.javabsic;
 
+import java.util.HashSet;
+
 public class recursionquestion2 {
     public static void move(String str,int i,int count,String result){
 
@@ -19,10 +21,56 @@ public class recursionquestion2 {
         }
 
     }
+    public static boolean[] map=new boolean[26];
+    public static void remove(String str,int i,String result){
+        if(i==str.length()){
+            System.out.println(result);
+            return;
+        }
+
+        char current=str.charAt(i);
+        if(map[current-'a']==true){
+            remove(str,i+1,result);
+        }else{
+            result+=current;
+            map[current-'a']=true;
+            remove(str,i+1,result);
+        }
+
+    }
+    public static void substring(String str,int i,String result){
+        if(i==str.length()){
+            System.out.println(result);
+            return;
+        }
+        char current=str.charAt(i);
+        substring(str,i+1,result+current);
+        substring(str,i+1,result);
+    }
+    public static void unniquesubstring(String str,int i,String result,HashSet<String> set1){
+        if(i==str.length()){
+            if(set1.contains(result)){
+                return;
+            }else{
+                System.out.println(result);
+                set1.add(result);
+                return;
+            }
+        }
+        char current=str.charAt(i);
+        unniquesubstring(str,i+1,result+current,set1);
+        unniquesubstring(str,i+1,result,set1);
+    }
 
     static void main(String[] args) {
         String str="axbcxxd";
+        String  str1="asdnniasaaajd";
+        String str2="aaa";
+        HashSet<String> set=new HashSet<>();
         move(str,0,0,"");
+        remove(str1,0,"");
+        substring(str2,0,"");
+        unniquesubstring(str2,0,"",set);
 
     }
 }
---
+++ src/com/JAVA_Programing/javabsic/recursionquestion3.java
@@ -0,0 +1,22 @@
+package com.JAVA_Programing.javabsic;
+
+public class recursionquestion3 {
+    public static String[] arr={".","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public static void combination(String str,int idx,String result){
+        if(idx==str.length()){
+            System.out.println(result);
+            return;
+        }
+
+        char current=str.charAt(idx);
+        String str1=arr[current-'0'];
+        for(int i=0;i<str1.length();i++){
+            combination(str,idx+1,result+str1.charAt(i));
+
+        }
+    }
+
+    static void main(String[] args) {
+        combination("23",0,"");
+    }
+}
