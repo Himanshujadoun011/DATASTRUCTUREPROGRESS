5a13a47a0f43604632b393a0ab40decaf048a1f5
Himanshujadoun <Himanshu.jadoun2024@glbajajgroup.org>
2026-06-18 23:57:33 +0530

    hi

---
+++ src/com/JAVA_Programing/javabsic/quicksort.java
@@ -0,0 +1,38 @@
+package com.JAVA_Programing.javabsic;
+
+import java.util.Arrays;
+
+//4,6,2,5,7,9,1,3
+public class quicksort {
+    public static int partition(int[] arr ,int start,int end){
+        int pivot=arr[end];
+        int i=start-1;
+        for (int j = start; j <end ; j++) {
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        i++;
+        int temp=arr[i];
+        arr[i]=pivot;
+        arr[end]=temp;
+        return i;
+    }
+    public static void quick(int[] arr,int start,int end){
+        if(start<end){
+            int pivot=partition(arr,start,end);
+            quick(arr,start,pivot-1);
+            quick(arr,pivot+1,end);
+        }
+
+    }
+
+    static void main(String[] args) {
+        int[] arr={4,6,2,5,7,9,1,3};
+        quick(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
