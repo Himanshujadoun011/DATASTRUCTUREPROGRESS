6e8f78cc2de22c47d44918e466c8eb23ad1d875c
Himanshujadoun <Himanshu.jadoun2024@glbajajgroup.org>
2026-06-23 01:04:56 +0530

    hi

---
+++ src/com/JAVA_Programing/javabsic/linked_list.java
@@ -0,0 +1,21 @@
+package com.JAVA_Programing.javabsic;
+
+import java.util.*;
+
+public class linked_list {
+    static void main(String[] args) {
+        LinkedList<String> list=new LinkedList<>();
+        list.addFirst("himasnhu");
+        list.add("Jadoun");
+        list.addLast(" GL");
+        list.addLast("Bajaj");
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+
+    }
+}
---
+++ src/com/JAVA_Programing/javabsic/linkwithoutcollection.java
@@ -0,0 +1,74 @@
+package com.JAVA_Programing.javabsic;
+
+public class linkwithoutcollection {
+    node head;
+    class node{
+         String data;
+         node next;
+         node(String data ){
+             this.data=data;
+             this.next=null;
+         }
+    }
+    public void addfirst(String data){
+        node n=new node(data);
+        if(head==null){
+            head=n;
+            return;
+        }
+        n.next=head;
+        head=n;
+    }
+    public void addlast(String data){
+        node n=new node(data);
+        if(head==null){
+            head=n;
+            return;
+        }
+        node current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=n;
+    }
+    public void print(){
+        node current=head;
+        while(current   !=null){
+            System.out.println(current.data);
+            current=current.next;
+        }
+    }
+    public void deletedirst (){
+        if(head==null){
+            return;
+        }
+        head=head.next;
+    }
+    public void deletelast(){
+        if(head==null){
+            return;
+        }
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        node secondlast=head;
+        node last=head.next;
+        while(last.next!=null){
+            last=last.next;
+            secondlast=secondlast.next;
+        }
+        secondlast.next=null;
+
+    }
+    public static void main(String[] args) {
+        linkwithoutcollection list=new linkwithoutcollection();
+        list.addfirst("Himanshu");
+        list.addlast("Jadoun");
+        list.addlast("DSA");
+        list.deletedirst();
+        list.print();
+
+    }
+
+}
